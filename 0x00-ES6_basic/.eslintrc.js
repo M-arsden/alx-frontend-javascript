9f9af3902c0dff145a489f28558d137d9b43c025
M-arsden <Marsden.ke@gmail.com>
2024-03-06 22:33:16 +0000

    wee

---
+++ 0x00-ES6_basic/.eslintrc.js
@@ -0,0 +1,11 @@
+module.exports = {
+  'root': true,
+  extends: [
+    "semistandard"
+  ],
+  rules: {
+    semi: ["error", "always"],
+    quotes: ["error", "double"]
+  }
+};
+Finally from the term
